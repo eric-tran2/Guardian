26073ccc7a1f58fc141767974a658aed50b0a453
Eric Tran <eric.tran119@gmail.com>
2021-07-29 23:07:30 -0400

    hard code predator path

--- src/index.js
+++ src/index.js
@@ -8,11 +8,11 @@ document.addEventListener('DOMContentLoaded', () => {
   // global variables
   const cellSize = 75;
   const cellGap = 3;
-  let startingGold = 300;
+  let startingGold = 100;
   const gameGrid = [];
   const predators = [];
   const predatorPositions = [];
-  const defenders = [];
+  const tanks = [];
   const projectiles = [];
   const turret1 = document.getElementById('turret')
   let frame = 0;
@@ -59,31 +59,63 @@ document.addEventListener('DOMContentLoaded', () => {
       }
     }
   }
-
-  function createRoad(){
-    const newArray = [
-      [0,1],[0,2],[0,3],[0,4],[0,5],[0,6],[0,7],[0,8],[0,9],[0,10],[0,11],[0,12],[0,13],[0,14],[0,15],[0,16],[0,17],[0,18],[0,19],[0,20],[0,21],[0,22],[0,23],[0,24],[0,25],[0,26],[0,27],
-      [1,1],
-      [2, 1], [2, 2], [2, 3], [2, 4], [2, 5], [2, 6], [2, 7], [2, 8], [2, 9], [2, 10], [2, 11], [2, 12], [2, 13], [2, 14], [2, 15], [2, 16], [2, 17], [2, 18], [2, 19], [2, 20], [2, 21], [2, 22], [2, 23], [2, 24], [2, 25], [2, 26],
-      [3,26],
-      [4, 7], [4, 8], [4, 9], [4, 10], [4, 11], [4, 12], [4, 13], [4, 14], [4, 15], [4, 16], [4, 17], [4, 18], [4, 19], [4, 20], [4, 21], [4, 22], [4, 23], [4, 24], [4, 25], [4, 26],
-      [5,7],
-      [6, 7], [6, 8], [6, 9], [6, 10], [6, 11], [6, 12], [6, 13], [6, 14], [6, 15], [6, 16], [6, 17], [6, 18], [6, 19], [6, 20], [6, 21], [6, 22], [6, 23], [6, 24], [6, 25], [6, 26],
-      [7,26],
-      [8,5],[8,6],[8, 7], [8, 8], [8, 9], [8, 10], [8, 11], [8, 12], [8, 13], [8, 14], [8, 15], [8, 16], [8, 17], [8, 18], [8, 19], [8, 20], [8, 21], [8, 22], [8, 23], [8, 24], [8, 25], [8, 26] 
+  
+  const createdRoad = 
+  [
+    [0,27],[0,26],[0,25],[0,24],[0,23],[0,22],[0,21],[0,20],[0,19],[0,18],[0,17],[0,16],[0,15],[0,14],[0,13],[0,12],[0,11],[0,10],[0,9],[0,8],[0,7],[0,6],[0,5],[0,4],[0,3],[0,2],[0,1],
+    [1,1],
+    [2, 1], [2, 2], [2, 3], [2, 4], [2, 5], [2, 6], [2, 7], [2, 8], [2, 9], [2, 10], [2, 11], [2, 12], [2, 13], [2, 14], [2, 15], [2, 16], [2, 17], [2, 18], [2, 19], [2, 20], [2, 21], [2, 22], [2, 23], [2, 24], [2, 25], [2, 26],
+    [3, 26],
+    [4, 26], [4, 25], [4, 24], [4, 23], [4, 22], [4, 21], [4, 20], [4, 19], [4, 18], [4, 17], [4, 16], [4, 15], [4, 14], [4, 13], [4, 12], [4, 11], [4, 10], [4, 9], [4, 8], [4, 7],
+    [5,7],
+    [6, 7], [6, 8], [6, 9], [6, 10], [6, 11], [6, 12], [6, 13], [6, 14], [6, 15], [6, 16], [6, 17], [6, 18], [6, 19], [6, 20], [6, 21], [6, 22], [6, 23], [6, 24], [6, 25], [6, 26],
+    [7,26],
+    [8, 27], [8, 26], [8, 25], [8, 24], [8, 23], [8, 22], [8, 21], [8, 20], [8, 19], [8, 18], [8, 17], [8, 16], [8, 15], [8, 14], [8, 13], [8, 12], [8, 11], [8, 10], [8, 9], [8, 8], [8, 7], [8, 6], [8, 5]
   ]
-    for (let i = 0; i < newArray.length; i++){
-      const [row,col] = newArray[i];
-      this.gameGrid[row][col].road = true;
-    }
-  }
+
 
   
-  function isRoad([x,y]){ // 360,0 
-    const coordToIdx = coord => Math.floor(coord/cellSize) 
-    return gameGrid[coordToIdx(y)][coordToIdx(x)].road
-  }
 
+
+  // function createRoad(){
+  //   // let roadMap = []
+  //   const newArray = [
+  //     [0,1],[0,2],[0,3],[0,4],[0,5],[0,6],[0,7],[0,8],[0,9],[0,10],[0,11],[0,12],[0,13],[0,14],[0,15],[0,16],[0,17],[0,18],[0,19],[0,20],[0,21],[0,22],[0,23],[0,24],[0,25],[0,26],[0,27],
+  //     [1,1],
+  //     [2, 1], [2, 2], [2, 3], [2, 4], [2, 5], [2, 6], [2, 7], [2, 8], [2, 9], [2, 10], [2, 11], [2, 12], [2, 13], [2, 14], [2, 15], [2, 16], [2, 17], [2, 18], [2, 19], [2, 20], [2, 21], [2, 22], [2, 23], [2, 24], [2, 25], [2, 26],
+  //     [3,26],
+  //     [4, 7], [4, 8], [4, 9], [4, 10], [4, 11], [4, 12], [4, 13], [4, 14], [4, 15], [4, 16], [4, 17], [4, 18], [4, 19], [4, 20], [4, 21], [4, 22], [4, 23], [4, 24], [4, 25], [4, 26],
+  //     [5,7],
+  //     [6, 7], [6, 8], [6, 9], [6, 10], [6, 11], [6, 12], [6, 13], [6, 14], [6, 15], [6, 16], [6, 17], [6, 18], [6, 19], [6, 20], [6, 21], [6, 22], [6, 23], [6, 24], [6, 25], [6, 26],
+  //     [7,26],
+  //     [8,5],[8,6],[8, 7], [8, 8], [8, 9], [8, 10], [8, 11], [8, 12], [8, 13], [8, 14], [8, 15], [8, 16], [8, 17], [8, 18], [8, 19], [8, 20], [8, 21], [8, 22], [8, 23], [8, 24], [8, 25], [8, 26] 
+  // ]
+  //   for (let i = 0; i < newArray.length; i++){
+  //     // const [row,col] = newArray[i];
+  //     // this.gameGrid[row][col].road = true;
+  //     newArray[i].road = true;
+  //   }
+  //   // console.log(newArray);
+  //   return newArray;
+  // }
+  
+  // // createRoad();
+  // const createdRoad = createRoad();
+  // for(let i = 0; i < createdRoad.length; i++){
+  //   console.log(createdRoad[i])
+  // }
+  
+  // function isRoad([x,y]){ // 360,0 
+  //   // const coordToIdx = coord => Math.floor(coord/cellSize) 
+  //   for(let i = 0; i < createdRoad.length; i++){
+  //     // console.log(createdRoad[i][0])
+  //     if(createdRoad[i][0] === x && createdRoad[i][1] === y) return true;
+  //   }
+  //   // return gameGrid[coordToIdx(y)][coordToIdx(x)].road
+  //   return false;
+  // }
+
+  // createRoad();
 
   class Predator {
     constructor(verticalPosition) {
@@ -91,19 +123,32 @@ document.addEventListener('DOMContentLoaded', () => {
       this.y = verticalPosition;
       this.width = cellSize;
       this.height = cellSize;
-      this.speed = Math.random() * 0.9 + 1;
+      this.speed = Math.random() * .001;
+      // this.speed = 
       this.movement = this.speed;
       this.health = 100;
       this.maxHealth = this.health;
+      this.i = 0;
+      this.ti = 0;
     }
 
 
     update() {
-      this.x -= this.movement;
+      // if(isRoad([this.x, this.y])){
+        // this.x -= this.movement;
+      // }
+      if(this.i < 120 && this.ti === 0){
+        this.x = (createdRoad[this.i][1] * this.width);
+        this.y = (createdRoad[this.i][0] * this.height);
+        this.i++;
+        this.ti = 30;
+      }else if( this.ti > 0){
+        this.ti--;
+      }
     }
 
     draw() {
-      ctx.fillStyle = 'green';
+      ctx.fillStyle = 'blue';
       ctx.fillRect(this.x, this.y, this.width, this.height);
       ctx.fillStyle = 'black';
       ctx.font = '30px Arial';
@@ -117,9 +162,11 @@ document.addEventListener('DOMContentLoaded', () => {
       predators[i].draw();
     }
     if (frame % 250 === 0){
-      let verticalPosition = Math.floor(Math.random() * 5) * cellSize;
-      predators.push(new Predator(verticalPosition));
-      predatorPositions.push(verticalPosition);
+      // let verticalPosition = Math.floor(Math.random() * 5) * cellSize;
+      // predators.push(new Predator(verticalPosition));
+      // predatorPositions.push(verticalPosition);
+      predators.push(new Predator(0));
+      predatorPositions.push(0);
     }
   }
   
@@ -131,17 +178,14 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   }
 
-  class Defender {
+  class Tank {
     constructor(x,y){
       this.x = x;
       this.y = y; 
-      // this.width = cellSize;
-      // this.height = cellSize;
-      this.width = 136;
-      this.height = 128;
+      this.width = cellSize;
+      this.height = cellSize;
       this.shoot = false;
       this.timer = 0;
-      // this.health = 100;
     }
     draw(){
       // ctx.fillStyle = 'blue';
@@ -161,22 +205,22 @@ document.addEventListener('DOMContentLoaded', () => {
   canvas.addEventListener('click', function(){
     const gridPositionX = mouse.x - (mouse.x % cellSize);
     const gridPositionY = mouse.y - (mouse.y % cellSize);
-    for (let i = 0; i < defenders.length; i++){
-      if (defenders[i].x === gridPositionX && defenders[i].y === gridPositionY){
+    for (let i = 0; i < tanks.length; i++){
+      if (tanks[i].x === gridPositionX && tanks[i].y === gridPositionY){
         return;
       }
     }
-    let defenderCost = 100;
-    if (startingGold >= defenderCost) {
-      defenders.push(new Defender(gridPositionX, gridPositionY));
-      startingGold -= defenderCost;
+    let tankCost = 100;
+    if (startingGold >= tankCost) {
+      tanks.push(new Tank(gridPositionX, gridPositionY));
+      startingGold -= tankCost;
     }
   })
 
-  function handleDefenders(){
-    for (let i = 0; i < defenders.length; i++){
-      defenders[i].draw();
-      defenders[i].update();
+  function handleTanks(){
+    for (let i = 0; i < tanks.length; i++){
+      tanks[i].draw();
+      tanks[i].update();
     }
   }
 
@@ -221,15 +265,14 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   }
 
-  console.log(gameGrid);
+  // console.log(gameGrid);
 
   function animate(){
     ctx.clearRect(0,0, canvas.width, canvas.height);
-    ctx.fillStyle = 'blue';
-    // ctx.arc(centerX, centerY, radius, 0, 2 * Math.PI, false);
+    // ctx.fillStyle = 'blue';
     handleGameGrid();
     handlePredators();
-    handleDefenders();
+    handleTanks();
     handleProjectiles();
     requestAnimationFrame(animate);
     frame++;
